db9557cd774eaf1d470e972f0cdcc3e3eb7c1a82
michael <gongxd666@gmail.com>
2017-04-17 16:23:01 +0800

    implement my posts

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,15 @@
+class Account::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, alert: "Group deleted"
+  end
+end
